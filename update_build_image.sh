1a1b15d2578eec32affe8cc1572a20f4abe8dff0
Kevin Greene <kgreenek@gmail.com>
2017-06-26 19:28:45 -0700

    Remove docker cleanup logic in update_build_image.sh

--- update_build_image.sh
+++ update_build_image.sh
@@ -6,12 +6,3 @@ docker build \
   --build-arg USER=${USER} \
   --build-arg USERID=${UID} \
   ${DIR}/docker/build
-
-# Cleanup images and containers without tags, to prevent it get from getting cluttered after running
-# this scipt a bunch of times.
-containers=( $(docker ps -aq 2>/dev/null) )
-docker rm "${containers[@]}" 2>/dev/null
-volumes=( $(docker ps --filter status=exited -q 2>/dev/null) )
-docker rm -v "${volumes[@]}" 2>/dev/null
-images=( $(docker images --filter dangling=true -q 2>/dev/null) )
-docker rmi "${images[@]}" 2>/dev/null
