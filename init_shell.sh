9ca935cf3223716b1ebec34e750569cd62881a4a
Kevin Greene <kgreenek@gmail.com>
2017-07-07 23:08:28 -0700

    Improve init_shell.sh robustness

--- init_shell.sh
+++ init_shell.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
-SCRIPT=$(readlink -f "$0")
-DIR=$(dirname "$SCRIPT")
-
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export LD_LIBRARY_PATH=$DIR/out/install/lib:$DIR/out/external/install/lib:$LD_LIBRARY_PATH
 export PATH=$DIR/out/install/bin:$PATH
